97bf5a94ef8c3922010a6f6c09f7928a80cf410e
Matei Dragutu <mateidr7@gmail.com>
2025-07-22 21:30:09 +0300

    asd

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -12,7 +12,7 @@ function Contact() {
   console.log(reff.current.offsetHeight)
   return (
     <section className="contact" ref={reff}>
-      <Image publicId={"Group_40_acmlfe"} width={width} height={reff.current.offsetHeight} className="imgs" />
+      <Image publicId={"Group_40_acmlfe"} width={width} height={reff.current.offsetHeight||380} className="imgs" />
       <h2 data-aos="fade-down">{t("contact_title")}</h2>
 
       <div className="row">
